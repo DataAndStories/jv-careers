663b96254197c75f46289005132ff23b2a7d7dfc
Arindam <arindam@ncoregames.com>
2022-10-14 21:40:43 +0530

    Added html and styles

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # FLASK WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
 # from flask module, import class Flask
-from flask import Flask
+from flask import Flask, render_template
 
 # create app, an object of class Flask
 app = Flask(__name__)
@@ -8,9 +8,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
-    print("I am inside if now")
     app.run(host="0.0.0.0", port=80, debug=True)
